d7bca768184b71b40a6814ede224bd618392c36a
zero1seven <zero1seven@pascal.local>
2015-10-29 12:32:24 -0500

    Added tor code. Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # get-tor
 A simple GET request using tor service.
+
+Requirements:
+    1. python 3
+    2. pycurl for python 3
+    3. tor services. The tor browser bundle is the simpliest implementation.
+
+In order for this code to connect to tor you must have tor services running on 127.0.0.1:9150 This code is to be used
+in conjection with my blog instructions at zero1seven.com.
---
+++ gettorinfo.py
@@ -0,0 +1,54 @@
+import io
+import sys
+import pycurl
+import re
+
+PORT = 9150
+
+def getbody(url):
+    'Connects to the torbundle tor service and returns data'
+    out = io.BytesIO()  #Will hold the IO output data
+    curl = pycurl.Curl()   #Uses pycurl to gain a connection through your tor services
+    curl.setopt(pycurl.URL, url)
+    curl.setopt(pycurl.PROXY, '127.0.0.1')
+    curl.setopt(pycurl.PROXYPORT, PORT)
+    curl.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
+    curl.setopt(pycurl.WRITEFUNCTION, out.write)  #Load the site data into out.write
+    
+    curl.perform() #HTTP request
+    curl.close()
+
+    return out.getvalue().decode("utf-8")  #The data needs converted from bytes to easily read.
+   
+
+def geturls(html):
+    'Takes a string and extracts url patterns'
+    urls = []
+
+    #Note that this regex pattern only grabs top level domains up to length 5. Other may exists but it will not catch them.
+    reg_obj = re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,5}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+    temp_domains = reg_obj.findall(html)
+
+    #There will be some empty strings returned which is filtered out here
+    if temp_domains != []:
+        for temp in temp_domains:
+            for tem in temp:
+                if tem != '':
+                    urls.append(tem)
+
+    return urls
+
+if __name__ == '__main__':
+    'prints the URLs html code or extracted URLs'
+    url = 'https://www.torproject.org/'
+    reg = None
+
+    if len(sys.argv) >= 2:  #If the command line supplies the first argument change the url 
+        url = sys.argv[1]
+    body = getbody(url)
+    if len(sys.argv) <= 2:   #Print the default or the provided URLs HTML code
+        print(body)
+    if  '-url' in sys.argv:  #Print all extracted URLs if the -url tag is present.
+        urls = geturls(body)
+        for url in urls:
+            print(url)
